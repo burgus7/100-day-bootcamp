1366f3ae0c18ca0c5f408e5e2cbc7c1886826ec8
Shweta <shweta.burgula@gmail.com>
2021-10-31 21:12:29 -0400

    Create Paddle class

--- day_22/pong_game.py
+++ day_22/pong_game.py
@@ -1,28 +1,21 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
 
-paddle = Turtle()
-paddle.penup()
-paddle.goto(350, 0)
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color("white")
+left_paddle = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 
 screen.exitonclick()
