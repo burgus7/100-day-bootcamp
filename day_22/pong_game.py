709fa3230193a7052f8d858af2dfba5c123298b7
Shweta <shweta.burgula@gmail.com>
2021-10-31 21:31:13 -0400

    animate ping pong ball

--- day_22/pong_game.py
+++ day_22/pong_game.py
@@ -1,21 +1,32 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
+screen.tracer(0)
 
 left_paddle = Paddle((-350, 0))
 right_paddle = Paddle((350, 0))
 
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
 screen.onkey(left_paddle.go_up, "w")
 screen.onkey(left_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 180 or ball.ycor() < -180:
+        ball.bounce()
 
 
 screen.exitonclick()
