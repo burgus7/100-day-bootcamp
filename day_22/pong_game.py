118d3845a2deefaa863a93159e25427ff5c84254
Shweta <shweta.burgula@gmail.com>
2021-10-31 21:03:34 -0400

    Paddle animation

--- day_22/pong_game.py
+++ day_22/pong_game.py
@@ -1,6 +1,28 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
+
+paddle = Turtle()
+paddle.penup()
+paddle.goto(350, 0)
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.color("white")
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+screen.exitonclick()
